6049edacb071710877cf737a2295482bcb2b1669
MohammedHafiz <mohammedhafizba@gmail.com>
2021-12-10 10:42:04 +0530

    check

--- Server/controller/auth.js
+++ Server/controller/auth.js
@@ -1,7 +1,7 @@
 const User = require ('../models/user');
-// const accountSid = process.env.TWILIO_ACCOUNT_SID;
-// const authToken = process.env.TWILIO_AUTH_TOKEN;
-// const serviceToken = process.env.TWILIO_SERVICE_TOKEN;
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const serviceToken = process.env.TWILIO_SERVICE_TOKEN;
 // const client = require('twilio')(accountSid, authToken);
 
 
