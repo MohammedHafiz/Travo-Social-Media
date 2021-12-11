1d4400d6c57cfb15eb474c72fc8269b558d9c4de
MohammedHafiz <mohammedhafizba@gmail.com>
2021-12-11 12:45:48 +0530

    updated user coll

--- Server/controller/auth.js
+++ Server/controller/auth.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcryptjs')
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const serviceToken = process.env.TWILIO_SERVICE_TOKEN;
+const asyncHandler = require('express-async-handler');
 
 // const client = require('twilio')(accountSid, authToken);
 
